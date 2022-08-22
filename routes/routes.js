b4b7d18f736dece5fef577e03b832a4110dbcded
budisf <budidarmawansyah81@gmail.com>
2022-08-22 06:38:21 -0700

    delete folder

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,5 @@ module.exports = app => {
 
      require(path.resolve('app/book/routes/routes'))(app)
      require(path.resolve('app/checkout/routes/routes'))(app)
-    // require(path.resolve('app/teacher/routes/routes'))(app)
 
     };
